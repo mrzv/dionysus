8c9cc91eb2e37d1dc6dd842282caea516c172768
Dmitriy Morozov <dmitriy@mrzv.org>
2014-09-05 08:21:51 -0700

    Streamlined ReducedMatrix: add() calls set() + use vector instead of set/list for intermediate representation

--- include/dionysus/reduced-matrix.hpp
+++ include/dionysus/reduced-matrix.hpp
@@ -5,26 +5,11 @@ dionysus::ReducedMatrix<F,I,C,V...>::
 add(const ChainRange& chain)
 {
     // TODO: skip the computation entirely if we already know this is positive (in case of the clearing optimization)
-    //std::list<Entry> c(std::begin(chain), std::end(chain));
-    //c.sort([this](const Entry& e1, const Entry& e2) { return this->cmp_(e1.index(), e2.index()); });
-    auto entry_cmp = [this](const Entry& e1, const Entry& e2) { return this->cmp_(e1.index(), e2.index()); };
-    std::set<Entry,decltype(entry_cmp)>     c(std::begin(chain), std::end(chain), entry_cmp);
+    Index i = pairs_.size();
+    pairs_.emplace_back();
+    reduced_.emplace_back();
 
-    visitors_chain_initialized(c);
-
-    Index pair = Reduction<Index>::reduce(c, reduced_, pairs_, field_,
-                                          [this](FieldElement m, Index cl)
-                                          { this->visitors_addto<>(m, cl); },
-                                          cmp_);
-
-    if (pair != unpaired)
-        pairs_[pair] = pairs_.size();
-
-    pairs_.push_back(pair);
-    reduced_.emplace_back(std::begin(c), std::end(c));
-    visitors_reduction_finished<>();
-
-    return pair;
+    return set(i, chain);
 }
 
 template<class F, typename I, class C, template<class Self> class... V>
@@ -33,20 +18,22 @@ typename dionysus::ReducedMatrix<F,I,C,V...>::Index
 dionysus::ReducedMatrix<F,I,C,V...>::
 set(Index i, const ChainRange& chain)
 {
-    std::list<Entry> c(std::begin(chain), std::end(chain));
-    c.sort([this](const Entry& e1, const Entry& e2) { return this->cmp_(e1.index(), e2.index()); });
+    auto entry_cmp = [this](const Entry& e1, const Entry& e2) { return this->cmp_(e1.index(), e2.index()); };
+    std::vector<Entry>  c(std::begin(chain), std::end(chain));
+    std::sort(c.begin(), c.end(), entry_cmp);
+
     visitors_chain_initialized(c);
 
     Index pair = Reduction<Index>::reduce(c, reduced_, pairs_, field_,
                                           [this](FieldElement m, Index cl)
                                           { this->visitors_addto<>(m, cl); },
-                                          cmp_);
+                                          entry_cmp);
 
     if (pair != unpaired)
         pairs_[pair] = i;
 
     pairs_[i]   = pair;
-    reduced_[i] = Chain(std::begin(c), std::end(c));
+    reduced_[i] = std::move(c);
     visitors_reduction_finished<>();
 
     return pair;
