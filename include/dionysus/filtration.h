0e2b2693a5e17dcdd82097460c072bbd0fed4734
Dmitriy Morozov <dmitriy@mrzv.org>
2014-12-08 14:33:16 -0800

    Added Filtration::add()

--- include/dionysus/filtration.h
+++ include/dionysus/filtration.h
@@ -60,6 +60,10 @@ class Filtration
         void                push_back(const Cell& s)                            { cells_.template get<order>().push_back(s); }
         void                push_back(Cell&& s)                                 { cells_.template get<order>().push_back(s); }
 
+        // return index of the cell, adding it, if necessary
+        size_t              add(const Cell& s)                                  { size_t i = index(s); if (i == size()) emplace_back(s); return i; }
+        size_t              add(Cell&& s)                                       { size_t i = index(s); if (i == size()) emplace_back(std::move(s)); return i; }
+
         // TODO: this is possibly incomplete (it seems to have trouble with multiple arguments); double-check
         template<class... Args>
         void                emplace_back(Args&&... args)                        { cells_.template get<order>().emplace_back(std::forward<Args>(args)...); }
