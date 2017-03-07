6ece621741348bc72e0ddcf18cd5a169fc956d5e
Dmitriy Morozov <dmitriy@mrzv.org>
2017-03-07 10:54:21 -0800

    Add documentation for Simplex bindings

--- bindings/python/simplex.cpp
+++ bindings/python/simplex.cpp
@@ -31,21 +31,24 @@ void init_simplex(py::module& m)
         .def("__iter__", [](PySimplexBoundaryIterator& it) -> PySimplexBoundaryIterator& { return it; })
         .def("__next__", &PySimplexBoundaryIterator::next);
 
-    py::class_<PySimplex>(m, "Simplex")
-        .def(py::init<>())
-        .def(py::init<std::vector<PySimplex::Vertex>>())
-        .def(py::init<std::vector<PySimplex::Vertex>, PySimplex::Data>())
+    py::class_<PySimplex>(m, "Simplex", "an abstract `simplex <https://en.wikipedia.org/wiki/Simplex>`_")
+        .def(py::init<>(), "construct empty simplex")
+        .def(py::init<std::vector<PySimplex::Vertex>>(), "construct from a list of vertices")
+        .def(py::init<std::vector<PySimplex::Vertex>, PySimplex::Data>(), "construct from a list of vertices and data")
         .def("__repr__",        [](const PySimplex& s) { std::ostringstream oss; oss << s << ' ' << s.data(); return oss.str(); })
-        .def("boundary",        [](py::object s) { return PySimplexBoundaryIterator(s.cast<const PySimplex&>(), s); })
-        .def("dimension",       &PySimplex::dimension)
-        .def("__len__",         &PySimplex::size)
-        .def("__getitem__",     &PySimplex::operator[])
+        .def("boundary",        [](py::object s) { return PySimplexBoundaryIterator(s.cast<const PySimplex&>(), s); },
+                                "returns iterator over the boundary of the simplex")
+        .def("dimension",       &PySimplex::dimension, "simplex dimension, one less than cardinality")
+        .def("__len__",         &PySimplex::size, "simplex cardinality")
+        .def("__getitem__",     &PySimplex::operator[], "access $i$-th vertex")
         .def("__iter__",        [](const PySimplex& s) { return py::make_iterator(s.begin(), s.end()); },
-                                py::keep_alive<0, 1>() /* Essential: keep object alive while iterator exists */)
-        .def("__contains__",    [](const PySimplex& s, PySimplex::Vertex v) { return std::find(s.begin(), s.end(), v) != s.end(); })
-        .def("join",            &PySimplex::join)
+                                py::keep_alive<0, 1>() /* Essential: keep object alive while iterator exists */,
+                                "iterator over the vertices")
+        .def("__contains__",    [](const PySimplex& s, PySimplex::Vertex v) { return std::find(s.begin(), s.end(), v) != s.end(); },
+                                "test whether the simplex contains given vertex")
+        .def("join",            &PySimplex::join, "join two simplices, i.e., take the union of their vertices")
         .def_property("data",   [](const PySimplex& s) { return s.data(); },
-                                [](PySimplex& s, PySimplex::Data x) { s.data() = x; })
+                                [](PySimplex& s, PySimplex::Data x) { s.data() = x; }, "access the data associated to the simplex")
         .def(py::self == py::self)
         .def(py::self != py::self)
         .def(py::self <  py::self)
