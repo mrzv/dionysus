e5ea18545bcb0c8561312d3e3aa067050c4288ad
Dmitriy Morozov <dmitriy@mrzv.org>
2017-03-08 13:26:56 -0800

    Add closure() to Python

--- bindings/python/dionysus/__init__.py
+++ bindings/python/dionysus/__init__.py
@@ -1,1 +1,15 @@
 from _dionysus import *
+
+def closure(simplices, k):
+    """Compute the `k`-skeleton of the closure of the list of `simplices`."""
+
+    res = set()
+
+    from    itertools   import combinations
+    for s in simplices:
+        for kk in range(1, k+2):
+            for face in combinations(list(s), min(s.dimension() + 1, kk)):
+                ss = Simplex(face)
+                res.add(ss)
+
+    return list(res)
