a94451319d728d1e0002381eaa365bfdbcba6b43
Dmitriy Morozov <dmitriy@mrzv.org>
2017-07-25 14:36:57 -0700

    Get rid of special treatment for Simplex.boundary(); use py::make_iterator(...) instead

--- bindings/python/simplex.cpp
+++ bindings/python/simplex.cpp
@@ -7,38 +7,17 @@ namespace py = pybind11;
 
 #include "simplex.h"
 
-// Need special handling of the boundary iterator
-struct PySimplexBoundaryIterator
-{
-    PySimplexBoundaryIterator(const PySimplex& s, py::object ref):
-        s(s), ref(ref), it(s.boundary_begin())  {}
-
-    PySimplex next()
-    {
-        if (it == s.boundary_end())
-            throw py::stop_iteration();
-        return *it++;
-    }
-
-    const PySimplex& s;
-    py::object ref;
-    PySimplex::BoundaryIterator it;
-};
-
 void init_simplex(py::module& m)
 {
     using namespace pybind11::literals;
-
-    py::class_<PySimplexBoundaryIterator>(m, "SimplexBoundaryIterator")
-        .def("__iter__", [](PySimplexBoundaryIterator& it) -> PySimplexBoundaryIterator& { return it; })
-        .def("__next__", &PySimplexBoundaryIterator::next);
 
     py::class_<PySimplex>(m, "Simplex", "an abstract `simplex <https://en.wikipedia.org/wiki/Simplex>`_")
         .def(py::init<>(), "construct empty simplex")
         .def(py::init<std::vector<PySimplex::Vertex>>(), "construct from a list of vertices")
         .def(py::init<std::vector<PySimplex::Vertex>, PySimplex::Data>(), "construct from a list of vertices and data")
         .def("__repr__",        [](const PySimplex& s) { std::ostringstream oss; oss << s << ' ' << s.data(); return oss.str(); })
-        .def("boundary",        [](py::object s) { return PySimplexBoundaryIterator(s.cast<const PySimplex&>(), s); },
+        .def("boundary",        [](const PySimplex& s) { return py::make_iterator(s.boundary_begin(), s.boundary_end()); },
+                                py::keep_alive<0, 1>() /* Essential: keep object alive while iterator exists */,
                                 "returns iterator over the boundary of the simplex")
         .def("dimension",       &PySimplex::dimension, "simplex dimension, one less than cardinality")
         .def("__len__",         &PySimplex::size, "simplex cardinality")
