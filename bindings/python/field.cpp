b57c6ff822abfba31d5e9f838ac590a03b0ddc8c
Dmitriy Morozov <dmitriy@mrzv.org>
2025-04-25 16:01:15 -0700

    Add bindings for various methods of Zp

--- bindings/python/field.cpp
+++ bindings/python/field.cpp
@@ -7,9 +7,20 @@ namespace py = pybind11;
 
 void init_field(py::module& m)
 {
+    using namespace pybind11::literals;
     py::class_<PyZpField>(m, "Zp", "arithmetic mod p")
         .def(py::init<PyZpField::Element>())
         .def("__repr__",        [](const PyZpField& f) { std::ostringstream oss; oss << "Z mod " << f.prime(); return oss.str(); })
+        .def("id",              &PyZpField::id,                      "1")
+        .def("zero",            &PyZpField::zero,                    "0")
+        .def("init",            &PyZpField::init,     "a"_a,         "(a % p + p) % p")
+        .def("neg",             &PyZpField::neg,      "a"_a,         "-a")
+        .def("add",             &PyZpField::add,      "a"_a, "b"_a,  "a + b")
+        .def("inv",             &PyZpField::inv,      "a"_a,         "1/a")
+        .def("mul",             &PyZpField::mul,      "a"_a, "b"_a,  "a * b")
+        .def("div",             &PyZpField::div,      "a"_a, "b"_a,  "a / b")
+        .def("is_zero",         &PyZpField::is_zero,  "a"_a,         "a == 0")
+        .def("prime",           &PyZpField::prime,                    "p")
     ;
 
     py::class_<PyQElement>(m, "QElement", "rational number")
