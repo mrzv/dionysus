1beecc09adf327fa686ffd8d27b974e1e55966de
Dmitriy Morozov <dmitriy@mrzv.org>
2025-12-11 13:49:49 -0800

    Bump version to 2.1.6

---
+++ build-wheels.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm -r .venv
+for PYTHON_VERSION in 3.12 3.13 3.14; do
+  uv run --python $PYTHON_VERSION --with build python -m build
+done
+
+rm -r .venv
+for PYTHON_VERSION in 3.12 3.13 3.14; do
+  orb -m ubuntu-plucky uv run --python $PYTHON_VERSION --with build python -m build
+done
