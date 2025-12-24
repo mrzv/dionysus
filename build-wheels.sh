d9b549f23ecbc3aa4861d0b79e5c7dbebd946db6
Dmitriy Morozov <dmitriy@mrzv.org>
2025-12-24 17:52:25 -0500

    Lower version requirements for dependencies to support Python 3.9; bump version to 2.1.8

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 rm -r .venv
-for PYTHON_VERSION in 3.12 3.13 3.14; do
-  uv run --python $PYTHON_VERSION --with build python -m build
+for PYTHON_VERSION in 3.9 3.10 3.11 3.12 3.13 3.14; do
+  uv run --python $PYTHON_VERSION --with build python -m build || exit 1
 done
 
 rm -r .venv
-for PYTHON_VERSION in 3.12 3.13 3.14; do
-  orb -m ubuntu-plucky uv run --python $PYTHON_VERSION --with build python -m build
+for PYTHON_VERSION in 3.9 3.10 3.11 3.12 3.13 3.14; do
+  orb -m ubuntu-plucky uv run --python $PYTHON_VERSION --with build python -m build || exit 1
 done
