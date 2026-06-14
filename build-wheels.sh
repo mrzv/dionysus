acd84eea56b6e6f46f99af583857296405c338bc
Dmitriy Morozov <dmitriy@mrzv.org>
2026-06-14 15:44:26 -0700

    Update build-wheels.sh to automatically audit wheels

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+PROJECT_VERSION=$(uv run python -c 'import tomllib; print(tomllib.load(open("pyproject.toml", "rb"))["project"]["version"])') || exit 1
+
 rm -r .venv
 for PYTHON_VERSION in 3.10 3.11 3.12 3.13 3.14; do
   uv run --python $PYTHON_VERSION --with build python -m build || exit 1
@@ -9,3 +11,5 @@ rm -r .venv
 for PYTHON_VERSION in 3.10 3.11 3.12 3.13 3.14; do
   orb -m ubuntu-plucky uv run --python $PYTHON_VERSION --with build python -m build || exit 1
 done
+
+orb -m ubuntu-plucky sh -c 'for fn in "dist/dionysus-$1"-*-linux*.whl; do uvx auditwheel repair --wheel-dir dist/wheelhouse --plat manylinux_2_39_x86_64 "$fn"; done' sh "$PROJECT_VERSION" || exit 1
